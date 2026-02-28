88f5096660a35a05b3a6ca6b12479c08921aecf7
Riccardo <riccardo.taranto@icloud.com>
2026-02-28 10:12:31 +0100

    Refactor prompts for user input to include descriptive comments

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
-let km = prompt;
-let age = prompt;
+//il numero di chilometri che vuole percorrere e l'età del passeggero//
+
+let km = prompt ("Quanti Km devi percorrere?");
+let age = prompt ("Quanti anni hai?");
 
 km = parseFloat(km);
 age = parseInt(age);
 
-
+//il prezzo del biglietto è definito in base ai km (0.21 € al km)//
