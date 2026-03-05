9c408d5394fdcc06ac8be925bf7de536fd2f2237
Riccardo <riccardo.taranto@icloud.com>
2026-03-05 15:25:49 +0100

    Refactor prompt formatting and spacing for improved readability

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-let km = prompt ("Quanti Km devi percorrere?");
-let age = prompt ("Quanti anni hai?");
+let km = prompt("Quanti Km devi percorrere?");
+let age = prompt("Quanti anni hai?");
 
 km = parseFloat(km);
 age = parseInt(age);
@@ -8,12 +8,12 @@ age = parseInt(age);
 
 const prezzoPerKm = 0.21;
 let prezzoTotale = km * prezzoPerKm;
- //il prezzo del biglietto è definito in base ai km (0.21 € al km)//
+//il prezzo del biglietto è definito in base ai km (0.21 € al km)//
 
 if (age < 18) {
     prezzoTotale = prezzoTotale - (prezzoTotale * 0.2);
     //va applicato uno sconto del 20% per i minorenni//
-}else if (age > 65) {
+} else if (age > 65) {
     prezzoTotale = prezzoTotale - (prezzoTotale * 0.4);
     //va applicato uno sconto del 40% per gli over 65//
 }
